f2f4ccdd3b0fb93cb8aa1a4f257808fbb7dfc235
maoforero <maforeror@gmail.com>
2022-01-14 16:02:30 -0500

    creando routes

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const { router } = require('./src/routes');
+
+app.use('/', (req, res) => {
+    res.send("Home")
+})
+
+
 app.listen(PORT, (req, res) => {
     console.log(`**** It's Alive 🤖 http://localhost:${PORT} ****`);
 })
